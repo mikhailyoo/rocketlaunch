e9548b5ae04d0652831f7e01fc294c80d2ab6447
mikhailyoo <134826518+mikhailyoo@users.noreply.github.com>
2025-03-17 11:04:12 +0500

    lesson 5

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -2,11 +2,19 @@ extends RigidBody3D
 
 
 func _process(delta: float) -> void:
-	if Input.is_action_pressed("ui_accept"):
+	if Input.is_action_pressed("move_up"):
 		apply_central_force(basis.y * delta * 1000)
 		
-	if Input.is_action_pressed("ui_left"):
+	if Input.is_action_pressed("left"):
 		apply_torque(Vector3(0.0, 0.0, 1.0) * delta * 100)
 		
-	if Input.is_action_pressed("ui_right"):
+	if Input.is_action_pressed("right"):
 		apply_torque(Vector3(0.0, 0.0, -1.0) * delta * 100)
+
+
+func _on_body_entered(body: Node) -> void:
+	if "Victory" in body.get_groups():
+		print("Victory")
+		
+	if "Obstacle" in body.get_groups():
+		print("You Crashed!")
