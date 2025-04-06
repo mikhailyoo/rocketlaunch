8971b3d1be8c65ee7c57046f874d93f47b26f25a
mikhailyoo <mikhailyoo@gmail.com>
2025-04-06 14:59:58 +0500

    lesson 11

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -5,6 +5,7 @@ extends RigidBody3D
 @export var rotation_power: float = 100
 
 @onready var audio_victory: AudioStreamPlayer = $AudioVictory
+@onready var engine_sound: AudioStreamPlayer3D = $EngineSound
 
 
 var is_blocked: bool = false
@@ -12,6 +13,10 @@ var is_blocked: bool = false
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("move_up"):
 		apply_central_force(basis.y * delta * engine_power)
+		if not engine_sound.playing:
+			engine_sound.play()
+	else:
+		engine_sound.stop()
 		
 	if Input.is_action_pressed("left"):
 		apply_torque(Vector3(0.0, 0.0, 1.0) * delta * rotation_power)
@@ -42,6 +47,8 @@ func crushed():
 
 func _on_body_entered(body: Node) -> void:
 	if is_blocked == false:
+		engine_sound.stop()
+		
 		if "Victory" in body.get_groups():
 			call_deferred("victory", body.next_level)
 			
